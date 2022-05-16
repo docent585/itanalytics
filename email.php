cb23bc2ffe07733c984815d7045454b50d526dac
docent585 <docent585@gmail.com>
2022-05-16 17:51:04 +0600

    Изменил текст в разлделе учебный центр

--- email.php
+++ email.php
@@ -1,34 +1,46 @@
 <?php
-$to = "docent585@gmail.com";//Почтовый ящик на который будет отправленно сообщение
-  $subject = "Тема сообщения";//Тема сообщения
-  $message = "Message, сообщение!";//Сообщение, письмо
-  $headers = "Content-type: text/plain; charset=utf-8 \r\n";//Шапка сообщения, содержит определение типа письма, от кого, и кому отправить ответ на письмо
+
+$to =  getenv('SEND_MAIL_TO'); // Добавить в файле php-fpm.conf, в раздел [www] 'env[SEND_MAIL_TO] = <email>' и перезапустить службу fpm
+$subject = "Тема сообщения"; //Тема сообщения
+$message = "Message, сообщение!"; //Сообщение, письмо
+$headers = "From: noreply@itanalytics.kz \r\n" . "Content-type: text/html; charset=utf-8 \r\n"; //Шапка сообщения, содержит определение типа письма, от кого, и кому отправить ответ на письмо
+$question = 'Потенциальный клиент не написал сообщение )))';
+
 // Проверяем или метод запроса POST
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-		// Поочередно проверяем или были переданные параметры формы, или они не пустые
-		if(isset($_POST["username"]){
-			//Если параметр есть, присваеваем ему переданое значение
-			$name 		=trim(strip_tags($_POST["username"]));
-		}
-		if(isset($_POST["usernumber"]))
-		{
-			$number 	= trim(strip_tags($_POST["usernumber"]));
-		}
-		if (isset( $_POST["question"])) {
-			$question 	= trim(strip_tags($question));
-		}
-			// Формируем письмо
-			$message  = "<html>";
-				$message  .= "<body>";
-				$message  .= "Телефон: ".$number;
-				$message  .= "</body>";
-			$message  .= "</html>";
-			// Окончание формирования тела письма
-			// Посылаем письмо
-			mail ($to, $subject, $message, $headers);
-}
-else
-{
-	exit;
-} 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Поочередно проверяем или были переданные параметры формы, или они не пустые
+    if (isset($_POST["username"])) {
+            //Если параметр есть, присваеваем ему переданое значение
+            $name = trim(strip_tags($_POST["username"]));
+        }
+        if (isset($_POST["usernumber"])) {
+            $number = trim(strip_tags($_POST["usernumber"]));
+        }
+        if (isset($_POST["question"])) {
+                $question = trim(strip_tags($_POST["question"]));
+                if (empty($question)) {
+                        $question = 'Потенциальный клиент не задал вопрос )))';
+                }
+        }
+        // Формируем письмо
+        $message = "<html>";
+        $message .= "<body>";
+        $message .= "Телефон: ".$number;
+        $message .= "<br />";
+        $message .= "Имя: ".$name;
+        $message .= "<br />";
+        $message .= "Вопрос: ".$question;
+        $message .= "</body>";
+        $message .= "</html>";
+        // Окончание формирования тела письма
+        // Посылаем письмо
+        mail($to, $subject, $message, $headers);
+        // Редирект на домашнюю страницу
+        header("Location: https://itanalytics.kz/");
+        die();
+    }
+    else {
+        exit;
+  }
+
 ?>
